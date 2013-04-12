4b40a35a0e055120db0820e398671dbefa88d25b
Gowtham Ashok <gwty93@gmail.com>
2013-04-12 22:48:06 +0530

    Moving to GitHub

---
+++ amisharp.cpp
@@ -0,0 +1,76 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include<iostream>
+
+using namespace cv;
+
+int main( int argc, char** argv )
+{
+    Mat src, src_gray, dst;
+    int kernel_size = 3;
+    int scale = 1;
+    int delta = 0;
+    int ddepth = CV_16S;
+    int c;
+    // Load an image
+    src = imread( argv[1] );
+
+    //for demonstration
+    namedWindow("Demonstration");
+    imshow("Demonstration",src);
+    std::cout<<"Original Image"<<std::endl;
+    waitKey(0);
+
+
+    if( !src.data )
+    {
+        return -1;
+    }
+
+    // Remove noise by blurring with a Gaussian filter
+    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
+    std::cout<<"Image after Removing noise"<<std::endl;
+
+    imshow("Demonstration",src);
+    waitKey(0);
+
+    // Convert the image to grayscale
+    cvtColor( src, src_gray, CV_RGB2GRAY );
+    std::cout<<"Grayscale Image"<<std::endl;
+
+    imshow("Demonstration",src_gray);
+    waitKey(0);
+
+    // equalizing Histogram to enhance contrast
+    equalizeHist(src_gray, src_gray );
+    std::cout<<"Image after enhancing  contrast"<<std::endl;
+
+    imshow("Demonstration",src_gray);
+    waitKey(0);
+
+    // Apply Laplace function
+    Mat abs_dst;
+    Laplacian( src_gray, dst, ddepth, kernel_size, scale, delta, BORDER_DEFAULT );
+    int x,y;
+    convertScaleAbs( dst, abs_dst );
+    std::cout<<"Image after applying Laplace Function"<<std::endl;
+
+    imshow("Demonstration",abs_dst);
+    waitKey(0);
+
+    float count=0,value=0;
+    value=0;
+    for( y=0; y<abs_dst.rows; y++ )
+    {
+        const short* dsttemp = abs_dst.ptr<short>(y);
+        for(x=0; x<abs_dst.cols;x++ )
+        {
+                count++;
+                value+=dsttemp[x];
+        }
+    }
+    std::cout<<"The average of the edge intensity is "<<value/count<<std::endl;
+    return 0;
+}
